724381f9c2822146ce2246d9b3c9bc3f3e63a994
Joost Yervante Damad <joost@damad.be>
2017-05-08 13:52:50 +0200

    read/write

---
+++ src/xml/mod.rs
@@ -0,0 +1,7 @@
+// (c) 2017 Joost Yervante Damad <joost@damad.be>
+
+pub mod read;
+pub mod write;
+
+pub use self::read::*;
+pub use self::write::*;
---
+++ src/xml/write.rs
@@ -0,0 +1,54 @@
+// (c) 2017 Joost Yervante Damad <joost@damad.be>
+
+use std::io::Write;
+use std::fs::File;
+use std::path::Path;
+
+use data::*;
+use error::*;
+
+fn indent<T>(writer:&mut T, offset:usize) -> Result<()>
+    where T:Write
+{
+    for _ in 0..offset {
+        writer.write(b"  ")?;
+    }
+    Ok(())
+}
+
+fn write_fermentable<T>(writer:&mut T, v:&Fermentable, offset:usize) -> Result<()>
+    where T:Write
+{
+    indent(writer, offset)?;
+    writer.write(b"<FERMENTABLE>\n")?;
+    indent(writer, offset)?;
+    writer.write(b"</FERMENTABLE>\n")?;
+    Ok(())
+}
+
+fn write_fermentables<T>(writer:&mut T, v:&Vec<Fermentable>, offset:usize) -> Result<()>
+    where T:Write
+{
+    indent(writer, offset)?;
+    writer.write(b"<FERMENTABLES>\n")?;
+    for f in v {
+        write_fermentable(writer, f, offset+1)?;
+    }
+    indent(writer, offset)?;
+    writer.write(b"</FERMENTABLES>\n")?;
+    Ok(())
+}
+
+pub fn write<T>(writer:&mut T, set:&RecordSet) -> Result<()>
+    where T:Write
+{
+    match *set {
+        RecordSet::Empty => Ok(()),
+        RecordSet::Fermentables(ref v) => write_fermentables(writer, v, 0),
+    }
+}
+
+pub fn write_file(filename:&Path, set:&RecordSet) -> Result<()> {
+    let mut f = File::create(filename)?;
+    write(&mut f, set)
+}
