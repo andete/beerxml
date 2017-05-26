3586aaab6856e6fd08047508c75b93aeec9c0eb6
Joost Yervante Damad <joost@damad.be>
2017-05-26 12:53:47 +0200

    hop data structure

--- src/data.rs
+++ src/data.rs
@@ -10,7 +10,7 @@ pub enum RecordSet {
     Fermentables(HashMap<String, Fermentable>),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug,Serialize,Deserialize)]
 pub enum FermentableType {
     /// grain
     Grain,
@@ -19,6 +19,7 @@ pub enum FermentableType {
     /// liquid extract
     Extract,
     /// dry extract
+    #[serde(rename = "Dry Extract")]
     DryExtract,
     /// adjunct
     Adjunct,
@@ -56,9 +57,12 @@ impl FermentableType {
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Fermentable {
     /// name of the fermentable
-    #[serde(skip_serializing,skip_deserializing)]
+    #[serde(skip)]
     pub name: String,
+    /// version of the fermentable format (normally 1)
+    pub version: i64,
     /// type of fermentable
+    #[serde(rename = "type")]
     pub type_: FermentableType,
     /// amount in kg
     pub amount: f64,
@@ -117,4 +121,85 @@ pub struct Fermentable {
     /// type, otherwise this value is ignored
     #[serde(skip_serializing_if="Option::is_none")]
     pub ibu_gal_per_lb: Option<f64>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Hop {
+    // name of the hop
+    #[serde(skip)]
+    pub name:String,
+    /// version of the fermentable format (normally 1)
+    pub version:i64,
+    /// percent alpha of hops
+    pub alpha:f64,
+    /// amount of hops, in kg
+    pub amount:f64,
+    /// use of the hop
+    #[serde(rename="use")]
+    pub use_:HopUse,
+    /// time in minutes
+    pub time:i64,
+    /// notes
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub notes:Option<String>,
+    /// type of hop
+    #[serde(rename="type", skip_serializing_if="Option::is_none")]
+    pub type_:Option<HopType>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub form:Option<HopForm>,
+    /// percent beta of hops
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub beta:Option<f64>,
+    /// Hop Stability Index - percent of hop alpha lost in 6 months of storage
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub hsi:Option<f64>,
+    /// Place of origin for the hops
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub origin:Option<String>,
+    /// Substitutes that can be used for this hop
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub substitutes:Option<String>,
+    /// Humelene leven in percent
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub humeline:Option<f64>,
+    /// Caryophyllene leven in percent
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub caryophyllene:Option<f64>,
+    /// Cohumylone leven in percent
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub cohumulone:Option<f64>,
+    /// Myrcene leven in percent
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub myrcene:Option<f64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum HopUse {
+    Aroma,
+    Boil,
+    #[serde(rename="Dry Hop")]
+    DryHop,
+    #[serde(rename="First Wort")]
+    FirstWort,
+    Mash,
+}
+
+impl Default for HopUse {
+    fn default() -> HopUse {
+        HopUse::Aroma
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum HopType {
+    Bittering,
+    Aroma,
+    Both,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum HopForm {
+    Pellet,
+    Plug,
+    Leaf,
 }
