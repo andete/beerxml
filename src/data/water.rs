bc91b46063a4d5d9e6cdfd1b96267f80e3ad2c62
Joost Yervante Damad <joost@damad.be>
2017-06-02 11:26:23 +0200

    flesh out water

--- src/data/water.rs
+++ src/data/water.rs
@@ -11,4 +11,24 @@ pub struct Water {
     /// name of the misc item
     #[serde(skip)]
     pub name: String,
+    /// version of the record, should be 1
+    pub version: i64,
+    /// Volume of water to use in a recipe in liters
+    pub amount: f64,
+    /// the amount of Ca in ppm
+    pub calcium: f64,
+    /// the amount of HCO3 in ppm
+    pub bicarbonate: f64,
+    /// the amount of SO4 in ppm
+    pub sulfate: f64,
+    /// the amount of Cl in ppm
+    pub chloride: f64,
+    /// the amount of Na in ppm
+    pub sodium: f64,
+    /// the amount of Mg in ppm
+    pub magnesium: f64,
+    /// the pH of the water
+    pub ph: Option<f64>,
+    /// notes
+    pub notes: Option<String>,
 }
